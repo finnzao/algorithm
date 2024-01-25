d308f09db3fe757131e13ae470f529c693b14c89
finnzao <lipefelipe403@hotmail.com>
2024-01-24 23:11:20 -0300

    .

--- dynamicProgramming/canConstruct.js
+++ dynamicProgramming/canConstruct.js
@@ -1,16 +1,16 @@
 //function should return a boolean indicatin whether or not the 'target' can be constructed
 // by concatenating elements of the 'wordBank' array.
 
-
-
+// ShowOf ->  https://prnt.sc/UHWPY45LLflq
+// time ->Brute force https://prnt.sc/0yef1ntEbUnG
 // Irá fazer uma verificação se alguma letra do target existe no workBank  
 // será retornando depois que susbtrair todas as strings do paramentro array e se torna um array vazio
 // se alguma das operações filhos returna true , a operação pai irá retorna true  
 
-const x = 'abcdfe'
-console.log(x.slice(1))
 
-const canConstruct = (target, wordBank) => {
+
+const canConstruct = (target, wordBank, memo = {}) => {
+    if (target in memo) return memo[target]
     if (target === '') return true
 
 
@@ -20,13 +20,27 @@ const canConstruct = (target, wordBank) => {
             //corte para substrair da palavra as letras que já existem no banco de dados
             const suffix = target.slice(word.length);
             //função aninhada após remoção das letras da palavra
-            canConstruct(suffix, wordBank)
+            if (canConstruct(suffix, wordBank, memo) === true) {
+                memo[target] = true
+                return true
+            }
+
         }
     }
-
+    memo[target] = false
+    return false
 };
 
 
 
-//console.log(canConstruct("abcdef", ["ab", "abc", "cd", "def", "abcd"]))
+console.log(canConstruct("abcdef", ["ab", "abc", "cd", "def", "abcd"]))
+console.log(canConstruct("skateboard", ["bo", "rd", "skate", "oard"]))
+console.log(canConstruct("eeeeeeeeeeeeeeeeeeeeeeeeeeeef",[
+    "e",
+    "ee",
+    "eee",
+    "eeee",
+    "eeeee",
+    "eeeeee"
+]));
 
---
+++ dynamicProgramming/countConstruct.js
@@ -0,0 +1,24 @@
+// The function should return the number of ways that the 'target can be
+// constructed by concatenating elements of the 'wordBank' array
+// ( how many times true was returned)
+
+
+const countConstruct = (target, wordBank, memo = []) => {
+    if (target in memo) return memo[target]
+    if (target === '') return 1
+
+    let totalCount = 0;
+    for (let word of wordBank) {
+        //Verificação para ver se palavra não está no meio do array , e sim no começo  
+        if (target.indexOf(word) === 0) {
+            const numWaysForRest = countConstruct(target.slice(word.length, wordBank, memo))
+            totalCount += numWaysForRest
+        }
+    };
+
+
+    memo[target] = totalCount
+}
+
+console.log(countConstruct("abcdef", ["ab", "abc", "cd", "def", "abcd"]))
+console.log(countConstruct("skateboard", ["bo", "rd", "skate", "oard"]))
